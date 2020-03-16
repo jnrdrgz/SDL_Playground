9292580d9f96cdf78ef8c7a43ea85958e2024709
jnrdrgz <yosoyjjjuan@gmail.com>
2020-03-16 17:24:49 -0300

    an advance

--- animations/main.cpp
+++ animations/main.cpp
@@ -4,6 +4,7 @@
 #include "game.h"
 #include <vector>
 #include <algorithm> 
+#include <string>
 
 static LogSystem log_system = LogSystem();
 
@@ -15,6 +16,46 @@ enum BombAnimationState
 };
 
 bool clicked = false;
+
+class Sprite
+{
+private:
+    bool animated;
+    SDL_Texture* texture = nullptr;
+    std::vector<SDL_Rect> src_rct_v;    
+    SDL_Rect dst_rct = {200,200,200,200};
+    
+public:
+    Sprite(SDL_Renderer *r, bool animated, std::string file_name, int src_rows, int src_cols,
+        int src_width, int src_height){
+        SDL_Surface* tmp_srf = IMG_Load(file_name.c_str());
+        texture = SDL_CreateTextureFromSurface(r, tmp_srf);
+        SDL_FreeSurface(tmp_srf);
+
+        this->animated = animated;
+
+        int x = 0;
+        int y = 0;
+        int w = src_width;
+        int h = src_height;
+        for(int i = 0; i<src_rows; i++){
+            for(int j = 0; j<src_cols; j++){
+                SDL_Rect r = {x,y,h,w};
+                src_rct_v.push_back(r);
+                x+=w;
+            }
+            y+=h;
+        }
+    }
+
+    void draw(SDL_Renderer* r){
+        SDL_RenderCopyEx(r, texture, &src_rct_v[0], &dst_rct, 0, 0, SDL_FLIP_NONE);
+    }
+
+    bool is_animated(){
+        return animated;
+    }
+};
 
 class Explosion
 {
@@ -142,10 +183,22 @@ int main(int argc, char* args[])
     int frame = 0;
     int animationFrame = 0;
 
-    log_system.add_text("FPS", "", game.renderer);
+    
+    log_system.add_text("AVG_FPS", "", game.renderer);
+    log_system.add_text("AVG_DT", "", game.renderer);
+    log_system.add_text("DT", "", game.renderer);
+
+    float avgDt = 0;
+
+    Uint32 start_time_frame = 0;
+    Uint32 finish_time_frame = 0;
+    int dt = 0;
 
     while(game.running){
         SDL_RenderClear(game.renderer);
+
+        dt = (SDL_GetTicks() - finish_time_frame);
+        
 
         while(SDL_PollEvent(&game.event)){
             if(game.event.type == SDL_QUIT){
@@ -167,19 +220,31 @@ int main(int argc, char* args[])
 
         bomb.draw(game.renderer, animationFrame, (int)avgFPS);
 
-        log_system.update_text("FPS", std::to_string(avgFPS), game.renderer);
+        if(avgFPS != 0){
+            avgDt = 1.0/(float)avgFPS;
+        }
+
+        log_system.update_text("AVG_FPS", std::to_string(avgFPS), game.renderer);
+        log_system.update_text("AVG_DT", std::to_string(avgDt), game.renderer);
+        log_system.update_text("DT", std::to_string(dt), game.renderer);
+        
         log_system.draw(game.renderer);
+
 
         
         SDL_SetRenderDrawColor( game.renderer, 255, 255, 255, 255);
         SDL_RenderPresent(game.renderer);
 
+
+        finish_time_frame = SDL_GetTicks();
         frame++;
         animationFrame++;
         //printf("%d\n", animationFrame);
         if(animationFrame > (int)avgFPS){
             animationFrame = 0;
         }
+
+
     }
     
     game.close();
