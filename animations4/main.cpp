169b36d50a58624444f3239b16226f686c2de45f
jnrdrgz <yosoyjjjuan@gmail.com>
2020-04-15 16:52:41 -0300

    conitnuos animation finished

--- animations4/main.cpp
+++ animations4/main.cpp
@@ -36,8 +36,8 @@ public:
 
         dst.x = 100;
         dst.y = 100;
-        dst.w = w*3;
-        dst.h = h*4;
+        dst.w = w*2;
+        dst.h = h*2;
 
         src.x = 0;
         src.y = 0;
@@ -79,6 +79,54 @@ public:
 
 };
 
+struct SteadyImage
+{
+public:
+    void load_texture(SDL_Renderer* renderer, std::string file_name){
+        SDL_Surface* tmp_srf = IMG_Load(file_name.c_str());
+        if(!tmp_srf){
+            printf("Error loading surface: \n, %s\n", SDL_GetError());
+        }
+
+        texture = SDL_CreateTextureFromSurface(renderer, tmp_srf);
+        if(!texture){
+            printf("Error loading texture: \n, %s\n", SDL_GetError());
+        }
+
+        SDL_FreeSurface(tmp_srf);
+    }
+public:
+    SteadyImage(){
+
+    }
+    SteadyImage(SDL_Renderer* r){ //std::string file_name, int init_f, int w, int h){
+                        //int total_frames, int tfc, int w, int h){
+
+        dst.x = 100;
+        dst.y = 100;
+        dst.w = 32*2;
+        dst.h = 64*2;
+
+        src.x = 0;
+        src.y = 0;
+        src.w = 32;
+        src.h = 64;
+
+        load_texture(r, "maleBase/full/advnt_full.png");
+
+    }
+
+    void draw(SDL_Renderer* renderer){
+        SDL_RenderCopyEx(renderer, texture, &src, &dst, 0, 0, SDL_FLIP_NONE);
+    }
+
+public:
+    SDL_Rect dst, src;
+    SDL_Texture* texture = nullptr;
+  
+
+};
+
 struct Animation
 {
 public:
@@ -87,7 +135,7 @@ public:
         stepTimer.start();
         frame = 1;
         totalFrames = 6;
-        duration = 1000;
+        duration = 600;
         step = duration/totalFrames;
 
         printf("step: %d\n", step);
@@ -143,17 +191,65 @@ private:
     AnimationImage animationImage;
 };
 
+
+struct Sprite
+{
+
+
+};
+
 struct GameObject
 {
 public:
     GameObject(){
         //bool repite = true;
-        Uint32 duration = 4;//seconds
-        animation.set_duration(duration);
         //sprite.add("walking", repite)
+
+    }
+    GameObject(SDL_Renderer* renderer){
+        //bool repite = true;
+        //sprite.add("walking", repite)
+
+        steadyImage = SteadyImage(renderer);
+        animations["walking"] = Animation(renderer);
+    }
+
+    void update(){
+        if(!steady){
+            animations["walking"].update();
+        }
+    }
+
+    void draw(SDL_Renderer* renderer){
+        if(walking){
+            animations["walking"].draw(renderer);
+        } 
+
+        if(steady){
+            steadyImage.draw(renderer);
+        }
+    }
+
+    void hande_input(SDL_Event event){
+        if(event.type == SDL_KEYDOWN){
+            if(event.key.keysym.sym == SDLK_RIGHT){
+                walking = true;
+                steady = false;
+            }
+        }
+
+        if(event.type == SDL_KEYUP){
+            if(event.key.keysym.sym == SDLK_RIGHT){
+                walking = false;
+                steady = true;
+            }
+        }
     }
 public:
-    Animation animation;
+    std::unordered_map<std::string, Animation> animations;
+    SteadyImage steadyImage;
+    bool walking = false;
+    bool steady = true;
 
 };
 int main(int argc, char* args[])
@@ -161,7 +257,8 @@ int main(int argc, char* args[])
     Game game;
     game.init("test", 500, 500);
     log_system.init();
-    Animation animation(game.renderer);
+    //Animation animation(game.renderer);
+    GameObject animation(game.renderer);
     //animation.set_duration(1000);
 
     while(game.running){
@@ -180,6 +277,7 @@ int main(int argc, char* args[])
                 
                 }
             }
+            animation.hande_input(game.event);
         }
 
         animation.update();
