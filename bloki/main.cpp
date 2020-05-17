5cb42b5dd00cc8c2176a441c7a479cf9f0e0166d
jnrdrgz <yosoyjjjuan@gmail.com>
2020-05-17 11:29:20 -0300

    almost there bloki

--- bloki/main.cpp
+++ bloki/main.cpp
@@ -2,27 +2,62 @@
 #include "../log_system/log_system.h"
 #include "../SDL_Needs/game.h"
 #include "../timer.h"
+#include "../particles/vector2.h"
 #include <vector>
 
 static LogSystem log_system = LogSystem();
 
+bool rct_collide(SDL_Rect a, SDL_Rect b){
+    if( a.x < b.x + b.w &&
+        a.x + a.w > b.x &&
+        a.y < b.y + b.h &&
+        a.y + a.h > b.y)
+    {
+        return true;
+    }
+
+    return false;
+
+}
+
 struct Block
 {
 public:
-    Block(){}
-    Block(int x, int y, int w, int h, SDL_Color color){
+    Block(){
+        active = true;
+    }
 
+    Block(int x, int y, int w, int h, SDL_Color color){
+        active = true;
+
+        rct.x = x;
+        rct.y = y;
+        rct.w = w;
+        rct.h = h;
+
+        color = color;
+    }
+
+    void draw(SDL_Renderer* renderer){
+        if(active){
+            SDL_SetRenderDrawColor( renderer, color.r, color.g, color.b, 255);
+            SDL_RenderFillRect(renderer, &rct);    
+        }
     }
     
     SDL_Rect rct;
+    SDL_Color color;
+    bool active;
 };
 
 struct Pad
 {
 public:
-    Pad(){}
+    Pad(){
+        velocity.x = 0.0f;
+        velocity.y = 0.0f;
+    }
     Pad(int x, int y, int w, int h){
-
     }
 
     void draw(SDL_Renderer* renderer){
@@ -31,16 +66,21 @@ public:
     }
 
     void update(){
-
+        rct.x += velocity.x;
+        //rct.y += velocity.y;
     }
 
     SDL_Rect rct;
+    Vector2 velocity;
 };
 
 struct Ball
 {
 public:
-    Ball(){}
+    Ball(){
+        velocity.x = 0.0f;
+        velocity.y = 0.0f;
+    }
     Ball(int x, int y, int w, int h){
 
     }
@@ -50,8 +90,16 @@ public:
         SDL_RenderFillRect(renderer, &rct);    
     }
 
+    void update(){
+        rct.x += velocity.x;
+        rct.y += velocity.y;
+    }
+
     SDL_Rect rct;
+    Vector2 velocity;
+
 };
+
 
 struct BlocksGame
 {
@@ -71,13 +119,80 @@ public:
         ball.rct.h = ball.rct.w;
         ball.rct.x = pad.rct.x;
         ball.rct.y = pad.rct.y - ball.rct.h;
+
+        int bx = delimiter.x + 20;
+        int by = delimiter.y + 20;
+        int bw = delimiter.w / 20;
+        int bh = delimiter.h / 40;
+        for(int i = 0; i < 10; i++){
+            SDL_Color color = {255,0,0};
+            blocks.push_back(Block(bx,by,bw,bh,color));
+            bx += bw + 5;
+        }
     }
 
     void draw(SDL_Renderer* renderer){
         SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255);
         SDL_RenderDrawRect(renderer, &delimiter);
+        
+        for(auto block : blocks){
+            block.draw(renderer);
+        }
+
         pad.draw(renderer);
         ball.draw(renderer);
+
+    }
+
+    void handle_input(SDL_Event event){
+        if(event.type == SDL_KEYDOWN){
+            if(event.key.keysym.sym == SDLK_s){
+                ball.velocity.x = 2.0f;
+                ball.velocity.y = -2.0f;
+            }
+            if(event.key.keysym.sym == SDLK_RIGHT){
+                pad.velocity.x = 2.0f;
+            }
+            if(event.key.keysym.sym == SDLK_LEFT){
+                pad.velocity.x = -2.0f;
+            }
+        }
+        if(event.type == SDL_KEYUP){
+            if(event.key.keysym.sym == SDLK_RIGHT){
+                pad.velocity.x = 0.0f;
+            }
+            if(event.key.keysym.sym == SDLK_LEFT){
+                pad.velocity.x = 0.0f;
+            }
+        }
+    }
+
+    void update(){
+        pad.update();
+        ball.update();
+
+        if(ball.rct.x < delimiter.x ||
+            ball.rct.x + ball.rct.w > delimiter.x + delimiter.w){
+            ball.velocity.x *= -1;
+        }
+
+        if(ball.rct.y < delimiter.y ||
+            ball.rct.y + ball.rct.h > delimiter.y + delimiter.h){
+            ball.velocity.y *= -1;
+        }
+        
+        if(rct_collide(pad.rct, ball.rct)){
+            ball.velocity.y *= -1;
+            ball.rct.y = pad.rct.y - ball.rct.h - 1; 
+        }
+
+        for(auto& block : blocks){
+            if(rct_collide(block.rct, ball.rct)){
+                block.active = false;
+                ball.velocity.y *= -1;
+            }
+        }
+
     }
 
     Pad pad;
@@ -90,10 +205,10 @@ public:
 int main(int argc, char* args[])
 {
     Game game;
-    game.init("test", 500, 500);
+    game.init("test", 640, 480);
     log_system.init();
 
-    BlocksGame blocksGame(100,100,300,300);
+    BlocksGame blocksGame(70,50,400,400);
     while(game.running){
         
         SDL_RenderClear(game.renderer);
@@ -110,8 +225,10 @@ int main(int argc, char* args[])
                 
                 }
             }
+            blocksGame.handle_input(game.event);
         }
 
+        blocksGame.update();
         blocksGame.draw(game.renderer);
 
     
