4600cfd42e8fff264e87d2577c399041cc22ca14
jnrdrgz <yosoyjjjuan@gmail.com>
2020-05-29 00:24:25 -0300

    base modified

--- base/main.cpp
+++ base/main.cpp
@@ -2,18 +2,28 @@
 #include "../log_system/log_system.h"
 #include "../SDL_Needs/game.h"
 #include "../timer.h"
+#include <vector>
 
 static LogSystem log_system = LogSystem();
+
+const int screen_w = 640;
+const int screen_h = 480;
 
 int main(int argc, char* args[])
 {
     Game game;
-    game.init("test", 500, 500);
+    game.init("test", screen_w, screen_h);
     log_system.init();
 
+    
     while(game.running){
         
         SDL_RenderClear(game.renderer);
+
+        const Uint8 *kbstate = SDL_GetKeyboardState(NULL);
+        if(kbstate[SDL_SCANCODE_M]){
+            printf("Pressing M\n");
+        }
 
         while(SDL_PollEvent(&game.event)){
             if(game.event.type == SDL_QUIT){
